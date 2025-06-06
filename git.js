16175ee96dbaac75edaae9894ba647e79fa724ac
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-06-06 00:05:51 -0700

    upi done

--- git.js
+++ git.js
@@ -8,3 +8,6 @@ const footer ="footer added in our website";
 console.log(footer);
 const payment="integrated the payment";
 console.log(payment);
+
+const upi="integrated the upi"
+console.log(upi);
